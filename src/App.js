ec3ada3432fc66ae1b146ceab3e09863bb13b4b0
Hari Agus Permana <permanahariagus@gmail.com>
2021-06-04 10:59:44 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./assets/scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React is awesome</h1>
+    </div>
+  );
+}
+
+export default App;
